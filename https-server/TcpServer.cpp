e5b3b11bca5acd7e504ca3e8c8c51ff68b99bd7f
ItayFrisk <itay.manevych@gmail.com>
2025-05-11 15:01:03 +0300

    create the base of the tcp server

--- https-server/TcpServer.cpp
+++ https-server/TcpServer.cpp
@@ -1,10 +1,67 @@
 #include "TcpServer.h"
+#include <winsock2.h>
+#include <Windows.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+// link with ws_32 in order to use WSAGetLastError()
+#pragma comment(lib,"Ws2_32.lib")
+
 using namespace http;
 
 TcpServer::TcpServer()
 {
+	StartServer();
 }
 
 TcpServer::~TcpServer()
 {
+	CleanServer();
 }
+
+void TcpServer::exitWithError(const std::string& message)
+{
+	std::cout << message;
+	exit(EXIT_FAILURE);
+}
+
+void TcpServer::log(const std::string& message)
+{
+	std::cout << message << std::endl;
+}
+
+void TcpServer::StartServer()
+{
+	WORD wVersion = MAKEWORD(2, 0);
+	int startup = WSAStartup(wVersion, &m_wsaData);
+	if (startup != 0) {
+		exitWithError("WSAStartup failed. Error: " + std::to_string(WSAGetLastError()) + '\n');
+	}
+
+	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock == INVALID_SOCKET) {
+		exitWithError("Could not create socket. Error: " + std::to_string(WSAGetLastError()) + '\n');
+	}
+	m_socket = sock;
+}
+
+void TcpServer::CleanServer()
+{
+	int result = closesocket(m_socket);
+	if (result == SOCKET_ERROR) {
+		log("Failed to close socket. Error: " + std::to_string(WSAGetLastError()));
+	}
+
+	int cleanup_result = WSACleanup();
+	if (cleanup_result != 0) {
+		log("WSACleanup failed. Error: " + std::to_string(WSAGetLastError()));
+	}
+
+	if (result == SOCKET_ERROR || cleanup_result != 0) {
+		exitWithError("An error occured when trying to clean server.");
+	}
+
+	log("Server cleaned succesfully");
+}
+
--- https-server/TcpServer.h
+++ https-server/TcpServer.h
@@ -2,11 +2,21 @@
 #ifndef TCP_SERVER_INCLUDED
 #define TCP_SERVER_INCLUDED
 
+#include <WinSock2.h>
+#include <iostream>
+
 namespace http {
 	class TcpServer {
 		public:
 			TcpServer();
 			~TcpServer();
+		private:
+			void exitWithError(const std::string& message);
+			void log(const std::string& message);
+			void StartServer();
+			void CleanServer();
+			WSADATA m_wsaData;
+			SOCKET m_socket;
 	};
 }
 
